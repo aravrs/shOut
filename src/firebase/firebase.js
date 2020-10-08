0c6c7a2363c69e20425132d9843081da04978e0f
Aravindh RS <aravindh.ravishankar@gmail.com>
2020-10-08 22:29:49 +0530

    Added auth functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
-import app from "firebaase/app";
+import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import firebaseConfig from "./config";
+import firebaseConfig from "./config/config";
 
 class Firebase {
   constructor() {
@@ -9,6 +9,26 @@ class Firebase {
     this.app = app;
     this.app = app.auth();
     this.db = app.firestore();
+  }
+
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithUserAndPassword(
+      email,
+      password
+    );
+    return newUser.user.updateProfile({ displayName: name });
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.SignOut();
+  }
+
+  resetPassword(email) {
+    return this.auth.sendPasswordResetEmail(email);
   }
 }
 
